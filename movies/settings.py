86df50763ef5a20c83f225c2d2e235d95f6cd0e2
denniseugenemartin <denniseeugenemartin@gmail.com>
2022-08-14 08:37:55 -0400

    Added requirements.txt

--- movies/settings.py
+++ movies/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'movies/static'),
