ee071ebee84c2704c7371dd4c065c608ad0a7878
denniseugenemartin <denniseeugenemartin@gmail.com>
2022-08-14 10:29:19 -0400

    changed settings.py

--- movies/settings.py
+++ movies/settings.py
@@ -124,8 +124,7 @@ STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+STATICFILES_DIRS = ( os.path.join('static'), )
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
